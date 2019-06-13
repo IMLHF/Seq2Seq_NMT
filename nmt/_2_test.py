698aa88dffa929d478d770c5938f49e1c546636d
IMLHF <red_wind@foxmail.com>
2019-06-13 16:45:05 +0800

    # add train & test

---
+++ nmt/_2_test.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import sys
+import os
+from utils import misc_utils
+from FLAGS import PARAM
+
+def main(log_file, ckpt_dir): # test
+  pass
+
+if __name__ == '__main__':
+  main(*misc_utils.ini_task('test'))
