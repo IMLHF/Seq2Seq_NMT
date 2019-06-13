26aa6247b2d76c2d6a8c2881a67814ff402c71a3
IMLHF <red_wind@foxmail.com>
2019-06-13 16:44:45 +0800

    # update nmt_main

--- nmt/nmt_main.py
+++ nmt/nmt_main.py
@@ -9,18 +9,18 @@ import _2_test
 
 def main():
   # num_worker = PARAM.num_workers
-  misc_utils.printinfo("# Visible Devices to TensorFlow %s." % repr(tf.Session().list_devices()))
+  exp_dir, log_dir, summary_dir, ckpt_dir, log_file = misc_utils.ini_task('nmt_main')
 
-  exp_dir = os.path.join(PARAM.root_dir,'exp')
-  if not os.path.exists(exp_dir):
-    misc_utils.printinfo('Output directory "exp" not exist, creating...')
-
-  # print hparams
-  misc_utils.print_hparams(not PARAM.verbose_print_hparams)
-
-  _1_train.main()
-  _2_test.main()
-
+  _1_train.main(exp_dir=exp_dir,
+                log_dir=log_dir,
+                summary_dir=summary_dir,
+                ckpt_dir=ckpt_dir,
+                log_file=log_file)
+  _2_test.main(exp_dir=exp_dir,
+               log_dir=log_dir,
+               summary_dir=summary_dir,
+               ckpt_dir=ckpt_dir,
+               log_file=log_file)
 
 if __name__ == '__main__':
   tf.app.run(main=main, argv=sys.argv)
