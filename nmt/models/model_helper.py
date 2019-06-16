d48264e0d6428c739f1aa7a4ab98ab84a4bc7ce2
IMLHF <red_wind@foxmail.com>
2019-06-16 23:27:13 +0800

    update

--- nmt/models/model_helper.py
+++ nmt/models/model_helper.py
@@ -1,30 +1,32 @@
-from FLAGS import PARAM
-from models import gnmt_model, attention_model, vanilla_model
-from utils import vocab_utils
-from utils import dataset_utils
-import tensorflow as tf
 import collections
+import tensorflow as tf
+from models import gnmt_model, attention_model, vanilla_model
+
+from FLAGS import PARAM
+from utils import dataset_utils
+from utils import vocab_utils
 
 
 def get_model_creator():
-  if PARAM.model_type in ['gnmt','gnmt_current']:
-    model_creator = gnmt_model.GNMTModel
-  elif PARAM.model_type == 'standard_attention':
-    model_creator = attention_model.AttentionModel
-  elif PARAM.model_type == 'vanilla':
-    model_creator = vanilla_model.Model
-  else:
-    raise ValueError('Unknown model type %s.' %
-                     PARAM.model_type)
+  model_creator = vanilla_model.BaseModel
+  # if PARAM.model_type in ['gnmt','gnmt_current']:
+  #   model_creator = gnmt_model.GNMTModel
+  # elif PARAM.model_type == 'standard_attention':
+  #   model_creator = attention_model.AttentionModel
+  # elif PARAM.model_type == 'vanilla':
+  #   model_creator = vanilla_model.Model
+  # else:
+  #   raise ValueError('Unknown model type %s.' %
+  #                    PARAM.model_type)
   return model_creator
 
 
-class TrainModel(
-    collections.namedtuple("TrainModel", ("graph", "model", "train_set"))):
+class GraphModelDataset(
+    collections.namedtuple("GraphModelDataset", ("graph", "model", "dataset"))):
   pass
 
-def create_train_model(log_file, model_creator, scope='train'):
-  """Create train graph, model, and iterator."""
+def build_train_model(log_file, model_creator, scope='train'):
+  """Build train graph, model, and iterator."""
 
   graph = tf.Graph()
   with graph.as_default(), tf.container(scope):
@@ -38,6 +40,7 @@ def create_train_model(log_file, model_creator, scope='train'):
                                                             tgt_file,
                                                             src_vocab_table,
                                                             tgt_vocab_table)
+
     train_model = model_creator(log_file=log_file,
                                 mode=vanilla_model.BaseModel.train_mode,
                                 batch_inputs=train_set,
@@ -46,5 +49,5 @@ def create_train_model(log_file, model_creator, scope='train'):
                                 src_vocab_size=src_table_size,
                                 tgt_vocab_size=src_table_size,
                                 scope=scope)
-  return TrainModel(graph=graph, model=train_model, train_set=train_set)
+  return GraphModelDataset(graph=graph, model=train_model, dataset=train_set)
 
