80a1fea6c6572b6cb7637cc3d85be98405870171
IMLHF <red_wind@foxmail.com>
2019-06-16 23:27:34 +0800

    update import

--- nmt/_1_train.py
+++ nmt/_1_train.py
@@ -1,29 +1,35 @@
-import tensorflow as tf
-import sys
 from models import model_helper
+import os
+import sys
+import tensorflow as tf
+
+from FLAGS import PARAM
 from utils import dataset_utils
 from utils import misc_utils
-import os
-from FLAGS import PARAM
 
+def train():
+  pass
 
 def main(exp_dir,
          log_dir,
          summary_dir,
          ckpt_dir,
          log_file):  # train
-  model_creator = model_helper.get_model_creator()
-  train_model = model_helper.create_train_model(log_file, model_creator, PARAM.scope)
-  eval_model = model_helper.create_eval_model(log_file, model_creator, PARAM.scope)
+  # model_creator = model_helper.get_model_creator()
+  # gmd : graph, model, dataset
+  # train_gmd = model_helper.build_train_model(log_file, model_creator, PARAM.scope)
+  # eval_gmd = model_helper.build_eval_model(log_file, model_creator, PARAM.scope)
 
   # Preload data for sample decoding.
-  dev_src_file = "%s.%s" % (PARAM.dev_prefix, PARAM.src)
-  dev_tgt_file = "%s.%s" % (PARAM.dev_prefix, PARAM.tgt)
-  sample_src_data = dataset_utils.load_data(dev_src_file)
-  sample_tgt_data = dataset_utils.load_data(dev_tgt_file)
+  # dev_src_file = "%s.%s" % (PARAM.dev_prefix, PARAM.src)
+  # dev_tgt_file = "%s.%s" % (PARAM.dev_prefix, PARAM.tgt)
+  # sample_src_data = dataset_utils.load_data(dev_src_file)
+  # sample_tgt_data = dataset_utils.load_data(dev_tgt_file)
 
   # TensorFlow Model
-  config_proto = misc_utils.get_session_config_proto()
+  # config_proto = misc_utils.get_session_config_proto()
+
+  pass
 
 
 
