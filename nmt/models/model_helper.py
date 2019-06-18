cabdec5b13b8cdfb2865378630c294ca3aff9b07
IMLHF <red_wind@foxmail.com>
2019-06-19 00:26:57 +0800

    update model_helper

--- nmt/models/model_helper.py
+++ nmt/models/model_helper.py
@@ -1,53 +1,65 @@
 import collections
 import tensorflow as tf
-from models import gnmt_model, attention_model, vanilla_model
+import tensorflow.contrib as contrib
 
 from FLAGS import PARAM
 from utils import dataset_utils
 from utils import vocab_utils
 
 
-def get_model_creator():
-  model_creator = vanilla_model.BaseModel
-  # if PARAM.model_type in ['gnmt','gnmt_current']:
-  #   model_creator = gnmt_model.GNMTModel
-  # elif PARAM.model_type == 'standard_attention':
-  #   model_creator = attention_model.AttentionModel
-  # elif PARAM.model_type == 'vanilla':
-  #   model_creator = vanilla_model.Model
-  # else:
-  #   raise ValueError('Unknown model type %s.' %
-  #                    PARAM.model_type)
-  return model_creator
+def _single_rnn_cell(unit_type, num_units, forget_bias, droprate, mode,
+                     residual_connection=False, device_str=None, residual_fn=None,
+                     activation_fun='tanh'):
+  droprate = 0.0 if mode == PARAM.MODEL_INFER_KEY else droprate
+
+  if unit_type == "lstm":
+    single_cell = contrib.rnn.LSTMCell(num_units=num_units,
+                                       initializer=tf.contrib.layers.xavier_initializer(),
+                                       # state_is_tuple=True,
+                                       activation=activation_fun,
+                                       # use_peepholes=True,
+                                       forget_bias=forget_bias)
+  elif unit_type == "gru":
+    single_cell = contrib.rnn.GRUCell(num_units=num_units,
+                                      activation=activation_fun)
+  elif unit_type == "layer_norm_lstm":
+    single_cell = contrib.rnn.LayerNormLSTMCell(num_units=num_units,
+                                                forget_bias=forget_bias,
+                                                layer_norm=True)
+  elif unit_type == "nas":
+    single_cell = contrib.rnn.NASCell(num_units=num_units)
+  else:
+    raise ValueError("Unknow units type %s" % unit_type)
+
+  # Dropout
+  if droprate > 0.0:
+    single_cell = contrib.rnn.DropoutWrapper(
+      cell=single_cell,input_keep_prob=(1.0-droprate))
+
+  # Residual
+  if residual_connection:
+    single_cell = contrib.rnn.ResidualWrapper(
+      single_cell, residual_fn=residual_fn)
+
+  # Device
+  if device_str:
+    single_cell = contrib.rnn.DeviceWrapper(single_cell, device_str)
+
+  return single_cell
+
+def multiRNNCell(unit_type, num_units, num_layers, layer_start_residual,
+                 forget_bias, droprate, mode, num_gpus):
+  cell_list = []
+  for i in range(1,num_layers+1):
+    single_cell = _single_rnn_cell(unit_type,num_units,forget_bias,droprate,mode,
+                                   residual_connection=(i>=layer_start_residual),
+                                   device_str="/gpu:%d" % ((i-1)%num_gpus))
+    cell_list.append(single_cell)
+  if len(cell_list)==1:
+    return cell_list[0]
+  else:
+    return contrib.rnn.MultiRNNCell(cell_list)
 
 
-class GraphModelDataset(
-    collections.namedtuple("GraphModelDataset", ("graph", "model", "dataset"))):
-  pass
 
-def build_train_model(log_file, model_creator, scope='train'):
-  """Build train graph, model, and iterator."""
-
-  graph = tf.Graph()
-  with graph.as_default(), tf.container(scope):
-    src_file = "%s.%s" % (PARAM.train_prefix, PARAM.src)
-    tgt_file = "%s.%s" % (PARAM.train_prefix, PARAM.tgt)
-    vocab_tables = vocab_utils.create_vocab_tables(log_file)
-    src_vocab_table, tgt_vocab_table, src_table_size, tgt_table_size = vocab_tables
-
-    train_set = dataset_utils.get_batch_inputs_form_dataset(log_file,
-                                                            src_file,
-                                                            tgt_file,
-                                                            src_vocab_table,
-                                                            tgt_vocab_table)
-
-    train_model = model_creator(log_file=log_file,
-                                mode=vanilla_model.BaseModel.train_mode,
-                                batch_inputs=train_set,
-                                source_vocab_table=src_vocab_table,
-                                target_vocab_table=tgt_vocab_table,
-                                src_vocab_size=src_table_size,
-                                tgt_vocab_size=src_table_size,
-                                scope=scope)
-  return GraphModelDataset(graph=graph, model=train_model, dataset=train_set)
 
