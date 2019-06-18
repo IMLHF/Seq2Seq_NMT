e4c32a1ee53802fbf6d16d116fac53cda9197299
IMLHF <red_wind@foxmail.com>
2019-06-19 00:28:01 +0800

    add loss

--- nmt/utils/loss_utils.py
+++ nmt/utils/loss_utils.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
+from FLAGS import PARAM
 
 
-def cross_entropy_loss(logits, crossent, decoder_cell_outputs, target_output, target_sequence_length, time_major, batch_size):
+def cross_entropy_loss(logits, crossent, decoder_cell_outputs, target_output, target_sequence_length, batch_size):
   """Compute optimization loss."""
   target_output = target_output
-  if time_major:
+  if PARAM.time_major:
     target_output = tf.transpose(target_output)
-  time_axis = 0 if time_major else 1
+  time_axis = 0 if PARAM.time_major else 1
   max_time = target_output.shape[time_axis].value or tf.shape(target_output)[time_axis]
 
   # crossent = self._softmax_cross_entropy_loss(
@@ -14,7 +15,7 @@ def cross_entropy_loss(logits, crossent, decoder_cell_outputs, target_output, ta
 
   target_weights = tf.sequence_mask(
       target_sequence_length, max_time, dtype=tf.float32)
-  if time_major:
+  if PARAM.time_major:
     target_weights = tf.transpose(target_weights)
 
   loss = tf.reduce_sum(
