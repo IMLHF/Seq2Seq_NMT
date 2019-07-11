c34515caeb970ada878219f074f7c45c2257cc4e
IMLHF <red_wind@foxmail.com>
2019-07-11 22:05:22 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 echo $1
+if [ -d "exp/$1" ]; then
+date=`date`
+echo 'dir exist, mv to "'$1_$date'"'
+mv exp/$1 "exp/$1_$date"
+fi
 mkdir exp/$1
-python3 -m $1.nmt_main | tee exp/$1.log && echo ' '
+python3 -m $1.nmt_main 2>&1 | tee exp/$1/$1.log && echo ' '
